c23f1e2ee23188170055bb763a665a749d4f864c
sitthatot <sitthatot@gmail.com>
2025-01-22 15:37:17 +0700

    add payment entity

--- src/entities/order.entity.ts
+++ src/entities/order.entity.ts
@@ -1,34 +1,3 @@
-// import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
-// import { CancelStatus } from '../dto/create-order/create-order.dto';
-
-// @Entity()
-// export class Order {
-//   @PrimaryGeneratedColumn()
-//   order_id: number;
-
-//   @Column()
-//   customer_id: number;
-
-//   @Column({ type: 'timestamp' })
-//   order_date: Date;
-
-//   @Column()
-//   total_price: number;
-
-//   @Column()
-//   queue_number: number;
-
-//   @Column()
-//   status: string;
-
-//   @Column({
-//     type: 'enum',
-//     enum: CancelStatus,
-//     default: CancelStatus.RefundPending,
-//   })
-//   cancel_status: CancelStatus;
-// }
-
 import {
   Entity,
   PrimaryGeneratedColumn,
@@ -45,14 +14,14 @@ export class Order {
   @CreateDateColumn()
   order_date: Date;
 
+  @Column({ nullable: true })
+  total_price: number;
+
   @Column()
   queue_number: number;
 
   @Column({ default: 'รอทำ' }) // สถานะเริ่มต้นเป็น "รอทำ"
   status: string;
-
-  @Column({ nullable: true })
-  total_price: number;
 
   @Column({ nullable: true })
   customer_id: number; // ไอดีลูกค้า
--- src/entities/payment.entity.ts
+++ src/entities/payment.entity.ts
@@ -1,1 +1,32 @@
-export class Payment {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  ManyToOne,
+  JoinColumn,
+  OneToOne,
+} from 'typeorm';
+import { Order } from './order.entity';
+
+@Entity()
+export class Payment {
+  @PrimaryGeneratedColumn()
+  payment_id: number;
+
+  @OneToOne(() => Order, { nullable: false })
+  @JoinColumn({ name: 'order_id' })
+  order: Order;
+
+  @CreateDateColumn()
+  payment_date: Date;
+
+  @Column({ type: 'varchar', length: 255 })
+  payment_method: string;
+
+  @Column()
+  amount: number;
+
+  @Column()
+  total_amount: number;
+}
